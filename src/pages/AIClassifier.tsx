47569756de22194d5bd9fd3d43e9545e9e3019f2
Appleprojects <nooneshouldtouch9@gmail.com>
2025-03-07 18:06:46 +0530

    Update AIClassifier.tsx

--- src/pages/AIClassifier.tsx
+++ src/pages/AIClassifier.tsx
@@ -41,7 +41,7 @@ const AIClassifier = () => {
     formData.append("file", imageFile);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict/", {
+      const response = await fetch("https://foodproject-backend-y74b.onrender.com/predict/", {
         method: "POST",
         body: formData,
       });
